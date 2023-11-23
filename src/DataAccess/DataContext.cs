b8688cd2546694de891202f7649f714bfd2c3e4a
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-11-23 20:40:35 +0400

    feat(DbContext) define

---
+++ src/DataAccess/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Ticketing.DataAccess.Entities;
+using Ticketing.DataAccess.Entities.Event;
+using Ticketing.DataAccess.Entities.Venue;
+
+namespace Ticketing.DataAccess;
+
+public class DataContext : DbContext
+{
+    private readonly IOptions<DatabaseSettings> options;
+
+    public DataContext(IOptions<DatabaseSettings> options)
+    {
+        this.options = options;
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Venue> Venues { get; set; }
+    public DbSet<Event> Events { get; set; }
+}
---
+++ src/DataAccess/DatabaseSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Ticketing.DataAccess;
+
+public class DatabaseSettings
+{
+    public required string ConnectionString { get; set; }
+    public required TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(10)
+}
