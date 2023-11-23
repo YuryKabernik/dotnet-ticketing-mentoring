b07f8a9e1b344f9b21db1439f0bfb186e5f2b271
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-11-23 20:25:22 +0400

    feat(Order, User) define models

--- src/DataAccess/Models/Order.cs
+++ src/DataAccess/Models/Order.cs
@@ -2,5 +2,7 @@
 
 public class Order
 {
-
+    public required int Id { get; set; }
+    public required int UserId { get; set; }
+    public required int SeatId { get; set; }
 }
--- src/DataAccess/Models/User.cs
+++ src/DataAccess/Models/User.cs
@@ -2,5 +2,9 @@
 
 public class User
 {
-
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public required string Email { get; set; }
+    public required string Phone { get; set; }
 }
