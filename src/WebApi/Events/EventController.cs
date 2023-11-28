d4b097114b85de6aba49556160981603a233f456
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-11-28 17:20:46 +0400

    feat(EventController) define mock API

---
+++ src/WebApi/Events/EventController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticketing.WebApi.Events.Models;
+
+namespace Ticketing.WebApi.Events;
+
+[Route("events")]
+[ApiController]
+public class EventController : ControllerBase
+{
+    public IEnumerable<OrganizedEvent> Events { get; set; }
+    public IEnumerable<EventSeat> Seats { get; set; }
+
+    public EventController()
+    {
+        this.Events = new List<OrganizedEvent>();
+        this.Seats = new List<EventSeat>();
+    }
+
+    [HttpGet]
+    public Task<IEnumerable<OrganizedEvent>> GetEvents()
+    {
+        return Task.FromResult(this.Events);
+    }
+
+    [HttpGet]
+    [Route("{eventId:int}/sections/{sectionId:int}/seats")]
+    public Task<IEnumerable<EventSeat>> GetSeats(int eventId, int sectionId)
+    {
+        return Task.FromResult(this.Seats);
+    }
+}
---
+++ src/WebApi/Events/Models/EventSeat.cs
@@ -0,0 +1,5 @@
+﻿namespace Ticketing.WebApi;
+
+public record EventSeat(int SectionId, int RowId, int SeatId, SeatStatus Status, SeatPriceOption PriceOption);
+public record SeatStatus(int Id, string Name);
+public record SeatPriceOption(int Id, string Name);
