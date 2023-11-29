5c6266e6a60a9fabe76bab804cf28529ec441040
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-11-29 23:02:35 +0400

    feat(Swagger) add API comments & messages

--- src/WebApi/Events/Models/OrganizedEvent.cs
+++ src/WebApi/Events/Models/OrganizedEvent.cs
@@ -1,3 +1,10 @@
 ﻿namespace Ticketing.WebApi.Events.Models;
 
+/// <summary>
+/// An event details contract.
+/// </summary>
+/// <param name="Id"></param>
+/// <param name="Name"></param>
+/// <param name="DateTime"></param>
+/// <param name="Place"></param>
 public record OrganizedEvent(int Id, string Name, DateTime DateTime, string Place);
