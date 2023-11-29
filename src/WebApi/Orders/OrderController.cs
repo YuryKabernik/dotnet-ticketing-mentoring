202f9069d41f19a53d4bbb04ae26e3c70b8cec58
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-11-29 23:02:35 +0400

    feat(Swagger) add API comments & messages

--- src/WebApi/Orders/OrderController.cs
+++ src/WebApi/Orders/OrderController.cs
@@ -4,17 +4,30 @@ using Ticketing.WebApi.Models;
 
 namespace Ticketing.WebApi;
 
+/// <summary>
+/// Describes Order resources.
+/// </summary>
 [Route("orders/carts/{cartId:guid}")]
 [ApiController]
 public class OrderController : ControllerBase
 {
     private readonly Dictionary<Guid, CartInfo> Carts;
 
+    /// <summary>
+    /// Initializes DI services.
+    /// </summary>
     public OrderController()
     {
         this.Carts = new Dictionary<Guid, CartInfo>();
     }
 
+    /// <summary>
+    /// Returns a list of items in a cart.
+    /// </summary>
+    /// <param name="cartId">
+    ///     A cart id is a uuid, generated and stored on the client side.
+    /// </param>
+    /// <returns></returns>
     [HttpGet]
     public Results<NotFound, Ok<CartInfo>> GetCart(Guid cartId)
     {
@@ -26,12 +39,27 @@ public class OrderController : ControllerBase
         return TypedResults.NotFound();
     }
 
+    /// <summary>
+    ///     Adds a seat to the cart.
+    /// </summary>
+    /// <param name="cartId">A cart uuid.</param>
+    /// <param name="seatDetails">An object of event_id, seat_id and price_id as a payload.</param>
+    /// <returns>
+    ///     Returns a cart state (with total amount) back to the caller.    
+    /// </returns>
     [HttpPost]
     public Results<BadRequest, Ok<CartInfo>> PostCart(Guid cartId, [FromBody] SeatSelection seatDetails)
     {
         return TypedResults.Ok(this.Carts[cartId]);
     }
 
+    /// <summary>
+    ///     Deletes a seat for a specific cart.
+    /// </summary>
+    /// <param name="cartId">A cart uuid.</param>
+    /// <param name="eventId"></param>
+    /// <param name="seatId"></param>
+    /// <returns></returns>
     [HttpDelete("events/{eventId:int}/seats/{seatId:int}")]
     public Results<NotFound, NoContent> DeleteSeat(Guid cartId, int eventId, int seatId)
     {
@@ -43,6 +71,13 @@ public class OrderController : ControllerBase
         return TypedResults.NotFound();
     }
 
+    /// <summary>
+    ///     Moves all the seats in the cart to a booked state.
+    /// </summary>
+    /// <param name="cartId">A cart uuid.</param>
+    /// <returns>
+    ///     Returns a payment id.
+    /// </returns>
     [HttpPut("book")]
     public Results<NotFound, Ok<PaymentInfo>> BookAll(Guid cartId)
     {
