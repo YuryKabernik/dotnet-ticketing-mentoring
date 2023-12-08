4dce5ee1e617a187d94e61257883fe67b6378527
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-12-08 18:21:13 +0400

    feat(Query) events requests

---
+++ src/Application/Feature/Event/EventsQuery.cs
@@ -0,0 +1,25 @@
+﻿using Ticketing.Application.CQRS;
+using Ticketing.Domain;
+using Ticketing.Domain.Entities.Event;
+
+namespace Ticketing.Application;
+
+public record EventsRequest();
+public record EventsResponse(IEnumerable<Event> Events);
+
+public class EventsQuery : IQueryHandler<EventsRequest, EventsResponse>
+{
+    private readonly IRepository<Event> repository;
+
+    public EventsQuery(IRepository<Event> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<EventsResponse> Execute(EventsRequest request)
+    {
+        var result = await this.repository.ListAsync();
+
+        return new EventsResponse(result!);
+    }
+}
---
+++ src/DataAccess/Queries/EventQuery.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Domain;
+using Ticketing.Domain.Entities.Event;
+
+namespace Ticketing.DataAccess;
+
+public class EventQuery : IRepository<Event>
+{
+    private readonly DataContext context;
+
+    public EventQuery(DataContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<Event?> FirstAsync(int id)
+    {
+        return await this.context.Events.SingleAsync(e => e.Id == id);
+    }
+
+    public async Task<IEnumerable<Event>?> ListAsync()
+    {
+        return await this.context.Events.ToArrayAsync();
+    }
+}
