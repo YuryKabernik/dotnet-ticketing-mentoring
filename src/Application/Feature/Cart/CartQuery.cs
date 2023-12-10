30c736efe7b0681adf8457c001ea007a16ff079c
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-12-10 16:57:04 +0400

    feat(Cart) get list of items in a cart

---
+++ src/Application/Feature/Cart/CartQuery.cs
@@ -0,0 +1,29 @@
+﻿using Ticketing.Application.CQRS;
+using Ticketing.Domain.Entities;
+using Ticketing.Domain.Entities.Event;
+using Ticketing.Domain.Interfaces;
+
+namespace Ticketing.Application;
+
+public record CartRequest(Guid CartId);
+public record CartResponse(Guid CartId, IEnumerable<EventSeat> Seats);
+
+public class CartQuery : IQueryHandler<CartRequest, CartResponse>
+{
+    private readonly IRepository<Cart, Guid> repository;
+
+    public CartQuery(IRepository<Cart, Guid> repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<CartResponse> ExecuteAsync(CartRequest request, CancellationToken cancellation)
+    {
+        var result = await this.repository.FirstAsync(request.CartId, cancellation);
+
+        return new(
+            result!.Guid,
+            result!.Seats
+        );
+    }
+}
---
+++ src/DataAccess/Repositories/CartRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Domain.Entities;
+using Ticketing.Domain.Interfaces;
+
+namespace Ticketing.DataAccess.Repositories;
+
+public class CartRepository : IRepository<Cart, Guid>
+{
+    private readonly DataContext context;
+
+    public CartRepository(DataContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<Cart?> FirstAsync(Guid cartId, CancellationToken cancellation)
+    {
+        return await this.context.Carts.SingleOrDefaultAsync(e => e.Guid == cartId, cancellation);
+    }
+
+    public async Task<IEnumerable<Cart>?> ListAsync(CancellationToken cancellation)
+    {
+        return await this.context.Carts.ToListAsync(cancellation);
+    }
+}
