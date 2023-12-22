bdbec2c845521826d1f1281a4d54bdfd91a692ca
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-12-22 23:47:42 +0400

    feat: payment by id query

---
+++ src/Application/Feature/Payments/PaymentByIdQuery.cs
@@ -0,0 +1,27 @@
+﻿using Ticketing.Application.CQRS;
+using Ticketing.Domain;
+
+namespace Ticketing.Application.Feature.Payments;
+
+public record PaymentByIdRequest(Guid PaymentId);
+public record PaymentStatusResponse(string Status);
+
+public class PaymentByIdQuery : IQueryHandler<PaymentByIdRequest, PaymentStatusResponse>
+{
+    private readonly IPaymentRepository paymentRepository;
+
+    public PaymentByIdQuery(IPaymentRepository paymentRepository)
+    {
+        this.paymentRepository = paymentRepository;
+    }
+
+    public async Task<PaymentStatusResponse> ExecuteAsync(PaymentByIdRequest request, CancellationToken cancellation)
+    {
+        var payment = await paymentRepository.GetAsync(request.PaymentId, cancellation);
+        NotFoundException.ThrowIfNull(payment!, $"Payment {request.PaymentId} is not found.");
+
+        var statusName = Enum.GetName(payment!.Status);
+
+        return new(statusName!);
+    }
+}
