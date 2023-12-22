c00403cbf8a6b97d27fcd4529054dbb5b82537fb
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2023-12-22 23:47:24 +0400

    feat: payment repository

---
+++ src/DataAccess/Repositories/PaymentRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ticketing.Domain;
+using Ticketing.Domain.Entities.Payments;
+
+namespace Ticketing.DataAccess;
+
+public class PaymentRepository : IPaymentRepository
+{
+    private readonly DataContext context;
+
+    public PaymentRepository(DataContext context)
+    {
+        this.context = context;
+    }
+
+    public Task<Payment?> GetAsync(Guid id, CancellationToken cancellation)
+    {
+        return this.context.Payments
+            .SingleOrDefaultAsync(payment => payment.PaymentGuid == id, cancellation);
+    }
+
+    public Task<Payment?> GetWithSeatsAsync(Guid id, CancellationToken cancellation)
+    {
+        return this.context.Payments
+            .Include(payment => payment.Order)
+            .ThenInclude(order => order!.Seats)
+            .SingleOrDefaultAsync(payment => payment.PaymentGuid == id, cancellation);
+    }
+}
---
+++ src/Domain/Interfaces/Repositories/IPaymentRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ticketing.Domain.Entities.Payments;
+
+namespace Ticketing.Domain;
+
+public interface IPaymentRepository
+{
+    public Task<Payment?> GetAsync(Guid id, CancellationToken cancellation);
+    public Task<Payment?> GetWithSeatsAsync(Guid id, CancellationToken cancellation);
+}
