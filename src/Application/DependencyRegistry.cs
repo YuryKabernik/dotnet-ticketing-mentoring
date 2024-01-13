f114aede98f9af9651f6fc19e49e9c0b0b15eb73
KobernykYura <yury.kabernik.berazouski@gmail.com>
2024-01-13 22:08:56 +0400

    feat(Application, DataAccess, webApi) register services

---
+++ src/Application/DependencyRegistry.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Ticketing.Application;
+
+public static class DependencyRegistry
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssemblyContaining(typeof(DependencyRegistry)));
+
+        return services;
+    }
+}
