934e836915c51c7adc4e745ee9eafbdd656536e8
KobernykYura <yury.kabernik.berazouski@gmail.com>
2024-01-13 22:07:09 +0400

    fix(Application, WebApi) include missing types

---
+++ src/Application/Feature/Event/Requests/EventSeatsBySectionRequest.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using Ticketing.Application.Feature.Event.Response;
+
+namespace Ticketing.Application.Feature.Event.Requests;
+
+public record EventSeatsBySectionRequest(int EventId, int SectionId) : IRequest<EventSeatsResponse>;
