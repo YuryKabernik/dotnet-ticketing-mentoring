f1a2e258d617a6aa3c74aa34ea662c06204698eb
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2024-01-29 19:28:23 +0400

    feat(EmailProvider) add polly retry policy

---
+++ src/Notification/HttpPolicies/RetryHttpPolicy.cs
@@ -0,0 +1,16 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+
+namespace Ticketing.Notification.Service.HttpPolicies;
+
+internal class RetryHttpPolicy
+{
+    /// <summary>
+    /// For the task it is enough to implement a 3-time retry policy with fixed time intervals.
+    /// </summary>
+    public static readonly IAsyncPolicy<HttpResponseMessage> Policy =
+        HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+            .WaitAndRetryAsync(retryCount: 3, retryAttempt => TimeSpan.FromSeconds(1.5));
+}
--- src/Notification/Program.cs
+++ src/Notification/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
 using Ticketing.Notification.Common;
+using Ticketing.Notification.Service.HttpPolicies;
 using Ticketing.Notification.Service.Providers.Email;
 using Ticketing.Notification.Service.Providers.Email.Interfaces;
 using Ticketing.Notification.Service.Settings;
@@ -11,13 +12,17 @@ builder.Services.Configure<EmailProviderConfiguration>(
 );
 
 builder.Services.AddNotifications(builder.Configuration);
-builder.Services.AddHttpClient<IEmailProvider, EmailProvider>((sp, client) =>
+builder.Services
+    .AddHttpClient<IEmailProvider, EmailProvider>(ConfigureEmailProvider)
+    .AddPolicyHandler(RetryHttpPolicy.Policy);
+
+var host = builder.Build();
+host.Run();
+
+static void ConfigureEmailProvider(IServiceProvider sp, HttpClient client)
 {
     var configuration = sp.GetRequiredService<IOptions<EmailProviderConfiguration>>().Value;
 
     client.BaseAddress = new Uri(configuration.BaseAddress);
     client.DefaultRequestHeaders.Authorization = configuration.AuthenticationHeader;
-});
-
-var host = builder.Build();
-host.Run();
+}
