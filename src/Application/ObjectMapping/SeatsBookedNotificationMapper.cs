682a1df4274f24718c7f1a843f24cbed7da9b3a6
Yury Kabernik-Berazouski <yury_kabernik-berazouski@epam.com>
2024-01-30 14:59:10 +0400

    feat(Application) introduce Mapperly mappings for NotificationMessage<EmailExpectingPayment>

---
+++ src/Application/ObjectMapping/SeatsBookedNotificationMapper.cs
@@ -0,0 +1,25 @@
+﻿using Riok.Mapperly.Abstractions;
+using Ticketing.Application.Feature.Carting.BookCartSeats.Notifications;
+using Ticketing.Domain.Entities;
+using Ticketing.Domain.Entities.Payments;
+using Ticketing.Notification.Common;
+using Ticketing.Notification.Common.Messages.Content;
+
+namespace Ticketing.Application.ObjectMapping;
+
+[Mapper]
+public static partial class SeatsBookedNotificationMapper
+{
+    [MapProperty(nameof(@SeatsBookedNotification.Order.User), nameof(@NotificationMessage<EmailExpectingPayment>.Content.Recipient))]
+    [MapProperty(nameof(@SeatsBookedNotification.Order.Payment), nameof(@NotificationMessage<EmailExpectingPayment>.Content.Order))]
+    public static partial NotificationMessage<EmailExpectingPayment> ToEmailMessage(this SeatsBookedNotification notification);
+
+    [MapProperty(nameof(User.Name), nameof(RecipientInfo.Name))]
+    [MapProperty(nameof(User.Email), nameof(RecipientInfo.Email))]
+    private static partial RecipientInfo ToRecipientInfo(this User user);
+
+    [MapProperty(nameof(@Payment.Order.Id), nameof(@RecipientsOrder.OrderId))]
+    [MapProperty(nameof(@Payment.PaymentGuid), nameof(@RecipientsOrder.PaymentId))]
+    [MapProperty(nameof(@Payment.Price), nameof(@RecipientsOrder.Amount))]
+    private static partial RecipientsOrder ToRecipientsOrder(this Payment payment);
+}
